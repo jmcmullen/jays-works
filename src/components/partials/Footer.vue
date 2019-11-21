13ed72010a907ac201baa5957c75b2f031839fae
Jay <jmcmullen@pm.me>
2019-11-22 00:42:16 +1100

    Add footer with CTA

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -12,23 +12,35 @@
         </a>
       </div>
     </div>
+
+    <div class="footer__bottom">
+      <div class="footer__content container">
+        <div class="footer__copyright">
+          &copy; {{ new Date().getFullYear() }} Jay McMullen. This website is made using Vue and
+          Gridsome. Hosted via Netlify. You can
+          <a class="footer__link" href="https://github.com/jmcmullen/jays-works" target="_blank"
+            >see the source code here</a
+          >.
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
   .footer {
     &__cta {
-      background-color: $color-black;
+      background-color: $color-light-black;
       padding: 2rem 0;
 
       &-container {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
-        align-items: center;
 
         @include desktop {
           flex-direction: row;
+          align-items: center;
         }
       }
     }
@@ -44,6 +56,25 @@
       color: $color-black !important;
       height: 40px;
       margin: 1rem 0 0.5rem;
+    }
+
+    &__link {
+      color: $color-white !important;
+      text-decoration: underline;
+    }
+
+    &__bottom {
+      color: $color-grey;
+      background-color: $color-black;
+    }
+
+    &__content {
+      padding: 2rem;
+
+      @include desktop {
+        display: flex;
+        justify-content: space-between;
+      }
     }
   }
 </style>
