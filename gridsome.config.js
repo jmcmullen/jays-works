e527785eb3a464a2be3432de2711b892dcf4be60
Jay <jmcmullen@pm.me>
2020-02-12 23:23:40 +1100

    replace insta with upwork

--- gridsome.config.js
+++ gridsome.config.js
@@ -47,7 +47,7 @@ module.exports = {
         shortName: 'Jay M',
         themeColor: '#1a1a1a',
         backgroundColor: '#ffffff',
-        icon: './android-chrome-512x515.png', // must be provided
+        icon: '.src/static/android-chrome-512x515.png',
         msTileImage: '',
         msTileColor: '#1a1a1a',
       },
--- src/components/pages/home/Socials.vue
+++ src/components/pages/home/Socials.vue
@@ -1,56 +1,81 @@
 <template>
   <div class="socials">
-    <a class="socials__link" href="https://github.com/jmcmullen" target="_blank">
+    <a
+      class="socials__link"
+      href="https://www.upwork.com/o/profiles/users/~01a20265fabe11c1dd/"
+      target="_blank"
+    >
+      <UpWork class="socials__icon" />
+    </a>
+    <a
+      class="socials__link"
+      href="https://github.com/jmcmullen"
+      target="_blank"
+    >
       <Github class="socials__icon" />
     </a>
-    <a class="socials__link" href="https://linkedin.com/in/j-mcmullen" target="_blank">
+    <a
+      class="socials__link"
+      href="https://linkedin.com/in/j-mcmullen"
+      target="_blank"
+    >
       <LinkedIn class="socials__icon" />
     </a>
-    <a class="socials__link" href="https://twitter.com/jaymcmullen10" target="_blank">
+    <a
+      class="socials__link"
+      href="https://soundcloud.com/jjmcmullen"
+      target="_blank"
+    >
+      <Soundcloud class="socials__icon" /> </a
+    ><a
+      class="socials__link"
+      href="https://twitter.com/jaymcmullen10"
+      target="_blank"
+    >
       <Twitter class="socials__icon" />
     </a>
+    <!-- 
     <a class="socials__link" href="https://instagram.com/jjmcmullen" target="_blank">
       <Instagram class="socials__icon" />
-    </a>
-    <a class="socials__link" href="https://soundcloud.com/jjmcmullen" target="_blank">
-      <Soundcloud class="socials__icon" />
-    </a>
+    </a> -->
   </div>
 </template>
 
 <script>
-  import Github from '../../../assets/images/icons/github.svg';
-  import LinkedIn from '../../../assets/images/icons/linkedin.svg';
-  import Twitter from '../../../assets/images/icons/twitter.svg';
-  import Instagram from '../../../assets/images/icons/instagram.svg';
-  import Soundcloud from '../../../assets/images/icons/soundcloud.svg';
+import Github from '../../../assets/images/icons/github.svg';
+import LinkedIn from '../../../assets/images/icons/linkedin.svg';
+import Twitter from '../../../assets/images/icons/twitter.svg';
+import Instagram from '../../../assets/images/icons/instagram.svg';
+import Soundcloud from '../../../assets/images/icons/soundcloud.svg';
+import UpWork from '../../../assets/images/icons/upwork.svg';
 
-  export default {
-    components: {
-      Github,
-      LinkedIn,
-      Twitter,
-      Instagram,
-      Soundcloud,
-    },
-  };
+export default {
+  components: {
+    Github,
+    LinkedIn,
+    Twitter,
+    Instagram,
+    Soundcloud,
+    UpWork,
+  },
+};
 </script>
 
 <style lang="scss">
-  .socials {
-    display: flex;
-    flex: 20 1 auto;
+.socials {
+  display: flex;
+  flex: 20 1 auto;
 
-    flex-direction: column;
-    justify-content: flex-end;
-    width: 3rem;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 3rem;
 
-    &__link {
-      margin-top: 0.5rem;
-    }
-
-    &__icon {
-      width: 24px;
-    }
+  &__link {
+    margin-top: 0.5rem;
   }
+
+  &__icon {
+    width: 24px;
+  }
+}
 </style>
