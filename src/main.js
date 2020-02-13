093dffacea0f790b34addfbeb48c4612b5c06dbf
Jay <jmcmullen@pm.me>
2020-02-13 14:50:55 +1100

    meta tags

--- src/main.js
+++ src/main.js
@@ -5,13 +5,32 @@ import DefaultLayout from '~/layouts/Default.vue';
 import '~/assets/styles/theme.scss';
 
 export default function(Vue, { router, head, isClient }) {
-  // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
 
-  // Google web fonts
   head.link.push({
     rel: 'stylesheet',
     href:
       'https://fonts.googleapis.com/css?family=Catamaran:400,700&display=swap',
+  });
+
+  head.meta.push({
+    key: 'og:description',
+    name: 'og:description',
+    content: `Freelance web developer based in Sydney, Australia with over 6 years of professional experience available for hire.`,
+  });
+
+  head.meta.push({
+    key: 'twitter:description',
+    name: 'twitter:description',
+    content: `Freelance web developer based in Sydney, Australia with over 6 years of professional experience available for hire.`,
+  });
+
+  router.beforeEach((to, _from, next) => {
+    head.meta.push({
+      key: 'og:url',
+      name: 'og:url',
+      content: process.env.GRIDSOME_BASE_PATH + to.path,
+    });
+    next();
   });
 }
